4e16e43712a520edfb05ef18d9264e25228ab14e
Mee14 <meelertleenutaphong@gmail.com>
2021-02-09 13:01:14 +0700

    Editing Flask file

--- FlaskCos.py
+++ FlaskCos.py
@@ -1,51 +1,26 @@
 from flask import Flask
 import random
+import mysql.connector
+
+mydb = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      passwd="",
+      database="Project"
+)
 
 app = Flask(__name__)
 
-#This return a random num
-@app.route("/getRandomNumber")
-def getRandomNumber():
-    
-    
-    myrandom = "<h1>" + r + "</h1>"
-    
-    return myrandom
+
 ######################################################
 @app.route("/")
 def home():
-    myhtml = '''
-<!DOCTYPE html>
-<html>
-<head>
-<script src="http://code.jquery.com/jquery-latest.min.js"></script>
-<script>
-setInterval(function()
-{
-    $.ajax({
-        type:"get",
-        url:"/getRandomNumber",
-        datatype:"html",
-        success:function(data)
-        {
-            //Whatever is returned by app.route("getTableContents") will be inside the variable "data"
-            //all the html inside of the <div class ="random"> will be replaced
-            $( ".random").html(data);
-        }
-    });
-}, 500);
-</script>
-</head>
-<body>
-<h1>Ajax Example</h1>
-
-Here is your table:
-<div class = "table">
-This is the table that will be replaced
-</div>
-</body>
-</html>
-'''
+    myhtml = ""
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM Customers")
+    for i in mycursor:
+        
+        myhtml = myhtml + str(i)
     return myhtml
 
 ###########################################################
