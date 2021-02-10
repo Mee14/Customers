37f60e9e9389e4e6233f62cf98fd050f467eea94
Mee14 <meelertleenutaphong@gmail.com>
2021-02-10 10:15:23 +0700

    adding datas into table

--- FlaskCos.py
+++ FlaskCos.py
@@ -8,21 +8,95 @@ mydb = mysql.connector.connect(
       passwd="",
       database="Project"
 )
-
+mycursor = mydb.cursor()
 app = Flask(__name__)
+#########################################################
+def createTable():
+    mycursor.execute("CREATE TABLE IF NOT EXISTS customers(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
 
 
+
+def insertonerow(x, y):
+    sql = "INSERT INTO IF NOT EXISTS customers (name, address) VALUES (%s,%s)"
+    val = (x, y)
+    mycursor.execute(sql, val)
+    
+    mydb.commit()
+    
+    print(mycursor.rowcount, "records inserted.")
+    
+
+
+def exist(name, address):
+    query = "SELECT count(*) FROM customers where name ='{}'".format(name)
+    
+    mycursor.execute(query)
+    
+    myresult = mycursor.fetchall()
+    
+    count = myresult[0][0]
+    print(count)
+    
+    if count == 0:
+        sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+        val = (name, address)
+        mycursor.execute(sql, val)
+        mydb.commit()
+        print(mycursor.rowcount, "records inserted.")
+    else:
+        print("duplicate")
+def update():
+    ID = input("Enter ID: ")
+    name = input("Enter new name: ")
+    address = input("Enter new address: ")
+    sql = "UPDATE customers SET name = '{}', address = '{}' where id = '{}'".format(name, address, ID)
+    mycursor.execute(sql)
+    mydb.commit()
+    print("Updated")
+    
+def remove():
+    name = input("Enter name of the element you are removing: ")
+    sql = "DELETE FROM customers where name = '{}'".format(name)
+    mycursor.execute(sql)
+    mydb.commit()
+    print("Deleted")
+    
+def insert():
+    ans = input("Enter name or 'remove'/'update': ")
+    if ans == "Done":
+        print("close")
+    elif ans == "update":
+        update()
+        ans = input("Enter 0 to go back")
+        
+        if ans == "0":
+            insert()
+    elif ans == "remove":
+        remove()
+        ans = input("Enter 0 to go back")
+        if ans == "0":
+            insert()
+    else:
+        name = ans
+        address = input("Enter address: ")
+        exist(name, address)
+        insert()
+#createTable()
+#insert()
 ######################################################
 @app.route("/")
 def home():
-    myhtml = ""
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM Customers")
+    
+    table = "<table style=\"width:100\">\n<tr>\n<th>ID</th>\n<th>Name</th>\n<th>Address</th>\n</tr>\n"
+    
+    mycursor.execute("SELECT * FROM Customers order by id")
+    
     for i in mycursor:
         
-        myhtml = myhtml + str(i)
+        table = table + "<tr>\n<td>{}</td>\n<td>{}</td>\n<td>{}</td>\n</tr>".format(i[0],i[1],i[2])
+        print(i)
         
-    return myhtml
+    return table
 
 ###########################################################
 if __name__=='__main__':
